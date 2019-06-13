c50b46671d03064550135b104161b579e2125909
Rachel <rachelhd.vo@gmail.com>
2019-06-13 14:31:37 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap")
+        .addEventListener("click", toggleMenu);
+
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    //åben burgermenu
+    document.querySelector("#menu")
+        .classList.toggle("hidden");
+
+    let erSkjult == document.querySelector("#menu")
+        .classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap")
+            .classList.toggle("change");
+    } else {
+        document.querySelector("#menuknap")
+            .classList.toggle("change");
+    }
+}
