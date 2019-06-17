6b5d05e4b582ce9583a4a5900fd0a30711369c83
Rachel <rachelhd.vo@gmail.com>
2019-06-17 12:02:35 +0200

    done styling index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 window.addEventListener("load", sidenVises);
 
+
 function sidenVises() {
     console.log("sidenVises");
 
     document.querySelector("#menuknap")
         .addEventListener("click", toggleMenu);
+
 }
 
 function toggleMenu() {
     console.log("toggleMenu");
 
     //åben burgermenu
-    document.querySelector("#menu")
-        .classList.toggle("hidden");
+    document.querySelector("#menu").classList.toggle("hidden");
 
     let erSkjult = document.querySelector("#menu")
         .classList.contains("hidden");
